11431388ff37cf7cb24fdaefa90462b916e7b21d
Avijit Biswas <71291040+avizyt@users.noreply.github.com>
2022-07-09 01:00:37 +0530

    two sum hasmap

--- src/com/avijit/LeetCodeDSA1/TwoSum.java
+++ src/com/avijit/LeetCodeDSA1/TwoSum.java
@@ -1,13 +1,14 @@
 package com.avijit.LeetCodeDSA1;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
     public static void main(String[] args) {
         int[] nums = {2,7,11,15};
         int[] nums2 = {3,2,4};
         int target = 6                               ;
-        int[] res = twoSum(nums2, target);
+        int[] res = twoSumHash(nums2, target);
         System.out.println(res[0] + " " + res[1]);
     }
 
@@ -29,15 +30,20 @@ public class TwoSum {
         return new int[]{start, end};
     }
 
-    public static int[] towSum2(int[] nums, int target){
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int s = 0;
-        int e = nums.length - 1;
-        while(s <= e){
-            if(map.containsValue(s){
-                map.put(s, nums[s]);
-
+    public static int[] twoSumHash(int[] nums, int target){
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];
+            if (map.containsKey(temp)){
+                return new int[] {
+                        map.get(temp), i
+                };
             }
+                else {
+                    map.put(nums[i], i);
+                }
+            }
+            return new int[] {};
         }
     }
-}
+
